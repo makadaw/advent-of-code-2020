588c11eb91031a9072c4bba67a906504bc76846f
makadaw <makadaw@gmail.com>
2020-12-05 13:10:08 +0100

    Add sorted variant for day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,14 @@
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<u32> {
+pub fn input_generator(input: &str) -> Vec<usize> {
     input
         .lines()
-        .flat_map(|s| s.parse::<u32>())
+        .flat_map(|s| s.parse::<usize>())
         .collect()
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &[u32]) -> u32 {
-    let sum: u32 = 2020;
+pub fn solve_part1(input: &[usize]) -> usize {
+    let sum: usize = 2020;
 
     for (i, i_val) in input.iter().enumerate() {
         for j_val in input[i..].iter() {
@@ -21,9 +21,26 @@ pub fn solve_part1(input: &[u32]) -> u32 {
     0
 }
 
+#[aoc(day1, part1, sorted)]
+pub fn solve_part1_sorted(input: &[usize]) -> usize {
+    let mut nums: Vec<usize> = input.to_vec();
+    nums.sort();
+
+    let mut a = 0;
+    let mut b = nums.len()-1;
+    while nums[a] + nums[b] != 2020 && a < b {
+        if nums[a] + nums[b] > 2020 {
+            b -= 1;
+        } else {
+            a += 1;
+        }
+    }
+    nums[a]*nums[b]
+}
+
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &[u32]) -> u32 {
-    let sum: u32 = 2020;
+pub fn solve_part2(input: &[usize]) -> usize {
+    let sum: usize = 2020;
 
     for (i, i_val) in input.iter().enumerate() {
         for (j, j_val) in input[i..].iter().enumerate() {
