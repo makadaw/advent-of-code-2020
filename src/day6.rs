38c6bb3511827b35b3bde521d7fb1a59f124f268
makadaw <makadaw@gmail.com>
2020-12-06 14:05:17 +0100

    Rust format day 6

--- src/day6.rs
+++ src/day6.rs
@@ -12,7 +12,11 @@ pub fn input_generator(input: &str) -> Vec<Vec<String>> {
 pub fn solve_part1(input: &[Vec<String>]) -> usize {
     input
         .iter()
-        .map(|g| g.iter().flat_map(|p| p.chars().collect::<Vec<char>>()).collect::<HashSet<char>>())
+        .map(|g| {
+            g.iter()
+                .flat_map(|p| p.chars().collect::<Vec<char>>())
+                .collect::<HashSet<char>>()
+        })
         .map(|m| m.len())
         .sum()
 }
@@ -23,7 +27,11 @@ pub fn solve_part2(input: &[Vec<String>]) -> usize {
         .iter()
         .flat_map(|g| {
             let mut items = g.iter().map(|p| p.chars().collect::<HashSet<char>>());
-            items.next().map(|set| items.fold(set, |set1, set2| set1.intersection(&set2).cloned().collect()))
+            items.next().map(|set| {
+                items.fold(set, |set1, set2| {
+                    set1.intersection(&set2).cloned().collect()
+                })
+            })
         })
         .map(|m| m.len())
         .sum()
