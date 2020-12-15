829b7f49aeef05c9c20020701bd877d04775218d
makadaw <makadaw@gmail.com>
2020-12-15 12:35:11 +0100

    Functional Day 14

--- src/day14.rs
+++ src/day14.rs
@@ -1,33 +1,48 @@
 use std::collections::HashMap;
 
-fn parse_mask(input: &str) -> (i64, i64) {
-    let (u, d) = input
-        .split(" = ")
-        .nth(1)
-        .unwrap()
-        .chars()
-        .fold((0, 0), |acc, c| match c {
-            '1' => ((acc.0 << 1) | 1, acc.1 << 1),
-            '0' => (acc.0 << 1, (acc.1 << 1) | 1),
-            _ => (acc.0 << 1, acc.1 << 1),
-        });
-    (u, !d)
+enum Instruction {
+    Mask(i64, i64),
+    Assign(usize, i64),
+}
+
+impl Instruction {
+    fn from(string: &str) -> Option<Instruction> {
+        if string.starts_with("mask") {
+            let (u, d) =
+                string
+                    .split(" = ")
+                    .nth(1)
+                    .unwrap()
+                    .chars()
+                    .fold((0, 0), |acc, c| match c {
+                        '1' => ((acc.0 << 1) | 1, acc.1 << 1),
+                        '0' => (acc.0 << 1, (acc.1 << 1) | 1),
+                        _ => (acc.0 << 1, acc.1 << 1),
+                    });
+            Some(Instruction::Mask(u, !d))
+        } else {
+            scan_fmt!(string, "mem[{d}] = {d}", usize, i64)
+                .map(|(idx, num)| Some(Instruction::Assign(idx, num)))
+                .unwrap_or(None)
+        }
+    }
 }
 
 #[aoc(day14, part1)]
 pub fn solve_part1(input: &str) -> i64 {
-    let mut mask: (i64, i64) = (0, 0);
-    let mut mem: HashMap<usize, i64> = HashMap::new();
-    for l in input.lines() {
-        if l.starts_with("mask") {
-            mask = parse_mask(l)
-        } else {
-            let (idx, num) = scan_fmt!(l, "mem[{d}] = {d}", usize, i64).unwrap();
-            mem.remove(&idx);
-            mem.insert(idx, (num | mask.0) & mask.1);
-        }
-    }
-    mem.values().sum()
+    let mem: HashMap<usize, i64> = HashMap::new();
+    let mem = input.lines().flat_map(|l| Instruction::from(l)).fold(
+        (mem, (0, 0)),
+        |(mut mem, mask), ist| match ist {
+            Instruction::Mask(u, l) => (mem, (u, l)),
+            Instruction::Assign(idx, num) => {
+                mem.remove(&idx);
+                mem.insert(idx, (num | mask.0) & mask.1);
+                (mem, mask)
+            }
+        },
+    );
+    mem.0.values().sum()
 }
 
 fn get_mem_vals(mask: &[char], value: usize) -> Vec<usize> {
@@ -43,21 +58,21 @@ fn get_mem_vals(mask: &[char], value: usize) -> Vec<usize> {
         }
     })
 }
-
 #[aoc(day14, part2)]
-pub fn p2(input: &str) -> usize {
-    let mut mem: HashMap<usize, usize> = HashMap::new();
-    let mut mask: Vec<char> = Vec::new();
-    for l in input.lines() {
-        if l.starts_with("mask") {
-            mask = l.split(" = ").nth(1).unwrap().chars().rev().collect();
+pub fn solve_part2(input: &str) -> usize {
+    let mem: HashMap<usize, usize> = HashMap::new();
+    let mem = input.lines().fold((mem, vec![]), |(mut mem, mask), line| {
+        if line.starts_with("mask") {
+            let mask = line.split(" = ").nth(1).unwrap().chars().rev().collect();
+            (mem, mask)
         } else {
-            let (target, value) = scan_fmt!(l, "mem[{d}] = {d}", usize, usize).unwrap();
+            let (target, value) = scan_fmt!(line, "mem[{d}] = {d}", usize, usize).unwrap();
             let ts = get_mem_vals(&mask, target);
             for t in ts {
                 mem.insert(t, value);
             }
+            (mem, mask)
         }
-    }
-    mem.values().sum()
+    });
+    mem.0.values().sum()
 }
